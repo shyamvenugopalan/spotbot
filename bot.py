74b04144da1d4ae90e064e7260adbb008aa34829
Shyam Nair <shyamvenugopalan@gmail.com>
2021-10-29 11:04:01 -0700

    Bug Fixes and Logging
    - Multiple tracks or list of tracks in messages now supported.
    - Tries to keep the order chronological.
    - Added logging
    - Added script I run on cloud to keep bot running.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import re
 import discord
 import spotipy
+import logging
 
 from spotipy.oauth2 import SpotifyOAuth
 from discord.ext import commands
@@ -10,6 +11,9 @@ from dotenv import load_dotenv
 
 MESSAGE_LIMIT = 5000 # Limit for searching message history in channel
 PLAYLIST_LIMIT = 500 # Limit for adding new songs to playlist
+
+# Logging configs
+logging.basicConfig(filename="spotbot.log", format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8', level=logging.INFO)
 
 '''
 Create .env file in the same directory and set the following environment variables.
@@ -19,6 +23,7 @@ SPOTIPY_CLIENT_ID='<spotify client id>'
 SPOTIPY_CLIENT_SECRET='<spotify client id>'
 SPOTIPY_REDIRECT_URI='http://localhost:9090'
 PLAYLIST_ID='<your playlist id>'
+LOG_FILE=''
 '''
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -29,6 +34,7 @@ bot = discord.ext.commands.Bot(command_prefix='!')
 
 @bot.command(name='build', help='Builds playlist from previous messages in channel.')
 async def build(ctx):
+    logging.info(f"build playlist called by {ctx.author}")
     await ctx.send('🤖: Building Playlist!')
 
     scope = ["playlist-modify-public","playlist-modify-private"]
@@ -38,7 +44,7 @@ async def build(ctx):
     playlist = sp.playlist_items(PLAYLIST_ID)
     for item in playlist["items"]:
         current_track_list.append(item['track']['id']) #get list of tracks already in playlist
-
+    logging.info(f"playlist contains {len(current_track_list)} tracks")
     def is_spotify_track_link(message):
         if len(message.content) == 0:
             return False
@@ -47,24 +53,25 @@ async def build(ctx):
         else:
             return False
 
-    def get_track_id(message):
-        id = str(re.search(".*spotify\.com\/track\/([\w-]{22}).*", message.content).group(1))
-        return id
+    def get_track_ids(message):
+        return re.findall(r'.spotify\.com\/track\/([\w-]{22}).', message.content)
 
     count = 0
-    async for msg in ctx.channel.history(limit=MESSAGE_LIMIT):
+    async for msg in ctx.channel.history(limit=MESSAGE_LIMIT, oldest_first=True):
         if msg.author != ctx.bot.user:
             if is_spotify_track_link(msg):
-                id = get_track_id(msg)
-                if id not in current_track_list:
-                    count = count+1
-                    print(f'Adding track: {id}')
-                    sp.playlist_add_items(PLAYLIST_ID, [id])
-                else:
-                    print(f'Skipping track: {id}')
-                # await ctx.send(f'🤖: Found Link {msg.content}')
-            if count == PLAYLIST_LIMIT:
+                ids = get_track_ids(msg)
+                for id in ids:
+                    if id not in current_track_list:
+                        count = count+1
+                        logging.info(f'Adding track: {id}')
+                        sp.playlist_add_items(PLAYLIST_ID, [id], position=0)
+                    else:
+                        logging.info(f'Skipping track: {id}')
+                    # await ctx.send(f'🤖: Found Link {msg.content}')
+            if count >= PLAYLIST_LIMIT:
                 break
+    logging.info(f'Added {count} new tracks')
     await ctx.send(f'🤖: Added {count} new tracks')
     await ctx.send(f'🤖: Playlist Link https://open.spotify.com/playlist/{PLAYLIST_ID}')
 
